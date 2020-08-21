2b9c87043455d686fd83dce5da58cfb0b2e18479
TLChilton <thomaschilton93@gmail.com>
2020-08-21 01:02:07 -0700

    Added comments

--- combinePdfs.py
+++ combinePdfs.py
@@ -1,10 +1,11 @@
 #! python3
-# combinePdfs.py
+# combinePdfs.py - combines all pdf files in a directory into a single pdf
 # Created by Thomas Chilton
 import PyPDF2, os, traceback
 from colorama import init
 init()
 
+# Collects all pdf file names into a list
 pdfFiles = []
 for filename in os.listdir('.'):
     if filename.endswith('.pdf'):
@@ -12,7 +13,9 @@ for filename in os.listdir('.'):
 pdfFiles.sort(key = str.lower)
 pdfWriter = PyPDF2.PdfFileWriter()
 
-if (pdfFiles):
+
+if (pdfFiles): # if pdfs exist
+    # Add each pdf to pdfWriter
     for filename in pdfFiles:
         print('Adding pdf file ' + filename)
         try:
@@ -23,11 +26,15 @@ if (pdfFiles):
                 pdfWriter.addPage(pageObj)
         except PyPDF2.utils.PdfReadError:
             print ('Error: ' + filename + ' is encrypted')
+
+    # Combined PDF file creation
     print('\u001b[95mPlease enter a name for the combined PDF file: \u001b[0m', end='')
     savedFile = input()
+    # If the user didn't include a .pdf extension add it at the end
     if savedFile.endswith('.pdf') == False:
         savedFile = savedFile + '.pdf'
     savedFile = os.path.abspath(savedFile)
+    # If the file name already exists ask the user for a new one
     while os.path.exists(savedFile):
         print('\u001b[95mERROR: ' + savedFile + ' already exists, \nPlease enter a new name: \u001b[0m', end= '')
         savedFile = input()
@@ -39,14 +46,14 @@ if (pdfFiles):
         pdfOutput = open(savedFile, 'wb')
         pdfWriter.write(pdfOutput)
         pdfOutput.close()
-    except:
+    except: # If the combined pdf fails to save then save the error report as a .txt
         errorFile = open('errorInfo.txt', 'w')
         errorFile.write(traceback.format_exc())
         errorFile.close()
         print('\u001b[91mFatal error encountered. Traceback info written to errorInfo.txt.\u001b[0m')
     print('Hit enter to close')
     input()
-else:
+else: # If no pdfs are found tell the user then end the program
     print('No pdfs found in ' + os.getcwd())
     print('Hit enter to close')
     input()
